bfbfe8c77c01f955bc361cd75541f301513c86cd
Serdar Özkan <serdar.ozkan@cmosrealitylab.com>
2020-03-10 15:12:29 +0300

    Add project files.

---
+++ RazorPagesLoginExample/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesLoginExample.Pages
+{
+    [Authorize]
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
